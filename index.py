e882cf62cab3ee3ad2cea437dc2a5debaac11aa7
BekaBitarashvili <bekabitarashvili@gmail.com>
2023-05-25 01:14:25 +0400

    Modified

--- index.py
+++ index.py
@@ -1,24 +1,60 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
+from flask import Flask, render_template
 
 
-bot = ChatBot("GeorgianAIChatbot", read_only=False, logic_adapters=["chatterbot.logic.BestMatch"])
+app = Flask(__name__)
+
+bot = ChatBot("GeorgianAIChatbot", read_only=False,
+              logic_adapters=[
+
+                  {
+                      "import_path": "chatterbot.logic.BestMatch",
+                      "default_response": "ამ კითხვაზე ჯერ არ მაქვს პასუხი :)",
+                      "maximum_similarity_threshold": 0.9
+                  }
+              ])
 
 list_to_train = [
-                "გამარჯობა",
-                "მოგესალმებით...",
-                "რა გქვია?",
-                "ჩემი სახელია ჩათ-ბოტი",
-                "რამდენი წლის ხარ?",
-                "მე ასაკი არ მაქვს",
-                "რა გინდა?",
-                "ჩემი მიზანია დავეხმარო ადამიანებს და ვუპასუხო მათ ყველა შეკითხვას"
+    "გამარჯობა",
+    "მოგესალმებით...",
+    "რა გქვია?",
+    "ჩემი სახელია ჩათ-ბოტი",
+    "რამდენი წლის ხარ?",
+    "მე ასაკი არ მაქვს",
+    "რა გინდა?",
+    "ჩემი მიზანია დავეხმარო ადამიანებს და ვუპასუხო მათ ყველა შეკითხვას",
+    "რამე საიტი მითხარი",
+    "აქ შედი https://google.com/"
+
+]
+list_to_train2 = [
+    "გამარჯობა",
+    "გაგიმარჯოს",
+    "რა გქვია?",
+    "ასისტენტი",
+    "რამდენი წლის ხარ?",
+    "მე შემქმნეს 2023 წელს...",
+    "რა გინდა?",
+    "მშვიდობა :) ... რა უნდა მინდოდეს გეხმარები..."
 
 ]
 
-list_trainer = ListTrainer(bot)
+# list_trainer = ListTrainer(bot)
+#
+# list_trainer.train(list_to_train)
+# list_trainer.train(list_to_train2)
 
-list_trainer.train(list_to_train)
-while True:
-    user_response = input("მომხმარებელი: ")
-    print("ჩათ-ბოტი: " + str(bot.get_response(user_response)))
+# while True:
+#     user_response = input("მომხმარებელი: ")
+#     print("ჩათ-ბოტი: " + str(bot.get_response(user_response)))
+
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
